63c31b4b86e0936494b0e07a344849177c7a0533
rponusz <v-rponusz@hotels.com>
2020-06-18 13:29:12 +0200

    installed react-router-dom and did basic routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+const PageOne = () => {
+    return <div>PageOne</div>
+}
+const PageTwo = () => {
+    return <div>PageTwo</div>
+}
 const App = () => {
-    return <div>App</div>
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/PageTwo" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App;
