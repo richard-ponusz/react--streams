2f3e02df689aba9a8165ba7b320649bf19a39654
richard-ponusz <ponuszrichard93@gmail.com>
2020-06-19 16:26:56 +0200

    fixed wrong clientId and renderd normal sign in/out buttons instead of texts

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -6,7 +6,7 @@ class GoogleAuth extends React.Component {
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
-                clientID: 
+                clientId: 
                     '815259190025-m7g25r5oo7d2sg907jup1rj8lmkb42eu.apps.googleusercontent.com',
                 scope: 'email'
             }).then(() => {
@@ -25,9 +25,19 @@ class GoogleAuth extends React.Component {
         if (this.state.isSignedIn === null) {
             return <div>I don't know if we are signed in.</div>
         } else if (this.state.isSignedIn) {
-            return <div>I am signed in.</div>
+            return (
+                <button className="ui red google button">
+                    <i className="google icon" />
+                    Sign out with Google
+                </button>
+            )
         } else {
-            return <div>I am not signed In.</div>
+            return (
+                <button className="ui red google button">
+                    <i className="google icon" />
+                    Sign in with Google
+                </button>
+            )
         }
     }
 
