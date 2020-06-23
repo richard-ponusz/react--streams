d1bbbe84e247e47e5e45b34db0b994db54a9758d
richard-ponusz <ponuszrichard93@gmail.com>
2020-06-23 12:11:21 +0200

    configured connect with redux-form, fixed undefined this and made a successfull post request

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -25,8 +25,8 @@ class StreamCreate extends React.Component {
         )
     }
 
-    onSubmit(formValues) {
-        console.log(formValues);
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues);
     }
 
     render() {
@@ -54,7 +54,9 @@ const validate = (formValues) => {
     return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate: validate
 })(StreamCreate);
+
+export default connect(null, { createStream})(formWrapped);
