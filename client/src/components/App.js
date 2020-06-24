4af5ae81ae0ab22b7b86586f10cad6258838dfab
richard-ponusz <ponuszrichard93@gmail.com>
2020-06-24 13:09:37 +0200

    created custom history object to use programmatical navigation for Action Creator

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+import history from '../history';
 
 import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
@@ -11,16 +12,16 @@ import StreamShow from './streams/StreamShow';
 const App = () => {
     return (
         <div className="ui container">
-            <BrowserRouter>
+            <Router history={history} >
                 <div>
                     <Header />
                     <Route path="/" exact component={StreamList} />
-                    <Route path="/streams/new" component={StreamCreate} />
-                    <Route path="/streams/edit" component={StreamEdit} />
-                    <Route path="/streams/delete" component={StreamDelete} />
-                    <Route path="/streams/show" component={StreamShow} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route path="/streams/delete" exact component={StreamDelete} />
+                    <Route path="/streams/show" exact component={StreamShow} />
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     )
 }
