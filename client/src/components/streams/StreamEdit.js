654cb1778cfca2feeefabcab23ff7c0ecf1474bd
richard-ponusz <ponuszrichard93@gmail.com>
2020-06-25 10:01:22 +0200

    passed form initialValues from edit component to the base, form component

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
     }
 
+    onSubmit = (formValues) => {
+        console.log(formValues);
+    }
+
     render() {
         if (!this.props.stream) {
             return <div>Loading...</div> 
         }
-        return <div>{ this.props.stream.title}</div>
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm
+                    initialValues={
+                        { 
+                            title: this.props.stream.title, 
+                            description: this.props.stream.description
+                        }
+                    }
+                    onSubmit={this.onSubmit} 
+                />
+            </div>
+        )
     }
 
 }
@@ -21,4 +39,4 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
