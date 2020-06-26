76be9cff3af18d95e0c6bf8be50d03e662353288
Richard Ponusz <richard_ponusz@epam.com>
2020-06-26 15:47:37 +0200

    cleaning up video ref for inital rendering

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -20,6 +20,10 @@ class StreamShow extends React.Component {
     componentDidUpdate() {
         this.buildPlayer();
     }
+
+    componentWillUnmount() {
+        this.player.destroy();
+    }
     
     buildPlayer() {
         if (this.player || !this.props.stream) {
